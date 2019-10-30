259e07fca4fb44f0a53ac9e62088436658e4dda6
Jorge Lopez <jorge.uriel1@gmail.com>
2019-10-30 16:13:35 -0600

    Fixed naming

--- API/API.py
+++ API/API.py
@@ -1,13 +1,4 @@
-import sys
-import datetime
-import time
-import jwt
-import paho.mqtt.client as mqtt
-import random
-import Adafruit_DHT
-import RPi.GPIO as GPIO
-from mfrc522 import SimpleMFRC522
-from Magnetic Sensor import switch_puerta
+from MagneticSensor import switch_puerta
 from RFIDReader import RFID_Read
 
 while(1):
