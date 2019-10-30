5135240337220bf44c195a38b49f0ffa875b9ac9
oscarmero96 <47573788+oscarmero96@users.noreply.github.com>
2019-10-30 15:39:32 -0600

    Funcion swtich magnetico puerta

--- sensors/Magnetic Sensor.py
+++ sensors/Magnetic Sensor.py
@@ -1,16 +1,15 @@
 import time
 import RPi.GPIO as io
 
-io.setmode(io.BCM)
+def switch_puerta():
+    io.setmode(io.BCM)
 
-door_pin = 4
+    door_pin = 4
 
-io.setup(door_pin, io.IN, pull_up_down=io.PUD_UP)
-
-while True:
-        if io.input(door_pin):
-            print("PUERTA ABIERTA!")
-            time.sleep(0.5)
-        else:
-            print ("PUERTA CERRADA!")
-            time.sleep(0.5)
+    io.setup(door_pin, io.IN, pull_up_down=io.PUD_UP)
+    if io.input(door_pin):
+        print("PUERTA ABIERTA!")
+        time.sleep(0.5)
+    else:
+        print ("PUERTA CERRADA!")
+        time.sleep(0.5)
