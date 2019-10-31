cbea77cd9ba970d10117e7d808c376a0e944bafb
Jorge Lopez <jorge.uriel1@gmail.com>
2019-10-31 16:06:54 -0600

    All Files2

--- API/API.py
+++ API/API.py
@@ -1,7 +1,7 @@
 from MagneticSensor import switch_puerta
 from RFIDReader import RFID_Read
 from TemperaturaDHT11 import temperatura
-
+from Camara import camara
 while(1):
     switch_puerta(3)
     RFID_Read()
