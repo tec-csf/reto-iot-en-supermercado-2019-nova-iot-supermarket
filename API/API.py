705d8b8e0d344b0dbda9fa5830c275ad3fe39dab
oscarmero96 <47573788+oscarmero96@users.noreply.github.com>
2019-10-31 09:33:53 -0600

    Funcion Temp y Camara

--- API/API.py
+++ API/API.py
@@ -1,8 +1,11 @@
 from MagneticSensor import switch_puerta
 from RFIDReader import RFID_Read
+from TemperaturaDHT11 import temperatura
 
 while(1):
     switch_puerta(3)
     RFID_Read()
+    temperatura(2)
+
     if estado_puerta == True and RFID_Read == True:
         print("ALERTA")
